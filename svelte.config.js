8dd17d4ebe84222220e8f5e6d0d57c3057c39296
diablo <progzone122@gmail.com>
2025-03-07 04:49:13 +0200

    hotfix base path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : "/progzone122/"
+			base: process.argv.includes('dev') ? '' : "/progzone122"
 		}
 	},
 
