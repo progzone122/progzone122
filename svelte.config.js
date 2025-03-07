670fba0d4f90b3787f08f091e653d8f7f4aa4fc3
diablo <progzone122@gmail.com>
2025-03-07 04:41:53 +0200

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : ""
 		}
 	},
 
