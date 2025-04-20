1cbc5c1fbdda5d295141c33bc3a1d6f301a55af1
DiabloSat <progzone122@gmail.com>
2025-04-20 06:06:50 +0300

    remove paraglide/i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	import SocialsComponent from "../components/SocialsComponent.svelte";
 	import MenuComponent from "../components/MenuComponent.svelte";
 	let { children } = $props();
 </script>
 
-<ParaglideJS {i18n}>
-	<SocialsComponent />
-	<MenuComponent />
-	<div class="py-[15em] pl-4">
-		{@render children()}
-	</div>
-</ParaglideJS>
+<SocialsComponent />
+<MenuComponent />
+<div class="py-[15em] pl-4">
+	{@render children()}
+</div>
